2a663c296f03de50c5ba81528368294191777e65
equus2001 <martinmunoz.alba@gmail.com>
2020-04-13 16:18:01 +0200

    Ejercicios bash

--- Ejercicios_Bash/Ejercicio5.sh
+++ Ejercicios_Bash/Ejercicio5.sh
@@ -2,13 +2,13 @@
 echo "Se va abrir uno de los anteriores ejercios"
 echo $1 $2 $3 $4
 if [ $1 == ejercicio1 ]; then
-./Ejercicio1.sh $2 $3
+	./Ejercicio1.sh $2 $3
 elif [ $1 == ejercicio2 ]; then
 	./Ejercicio2.sh $2 $3
-	elif [ $1 == ejercicio3 ]; then
-		./Ejercicio3.sh $2 $3 $4
-		elif [ $1 == ejercicio4 ]; then
-			./Ejercicio4.sh $2
-			else
-			echo "No hay más ejercicios"
+elif [ $1 == ejercicio3 ]; then
+	./Ejercicio3.sh $2 $3 $4
+elif [ $1 == ejercicio4 ]; then
+	./Ejercicio4.sh $2
+else
+	echo "No hay más ejercicios"
 fi
