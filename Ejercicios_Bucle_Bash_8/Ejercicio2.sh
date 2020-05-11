35a50e4548503dce8cfc674e67761d90873dbbe2
equus2001 <martinmunoz.alba@gmail.com>
2020-05-11 14:05:05 +0200

    Ejercicios Bucles Bash 8

--- Ejercicios_Bucle_Bash_8/Ejercicio2.sh
+++ Ejercicios_Bucle_Bash_8/Ejercicio2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 x=$1
 longitud=${#x}
-veces=`grep -o "a" <<< "$x" | wc -l`
-echo "Muestra el número de caracteres igual a -a- que contenga la palabra dada"
+veces=`grep -o "$2" <<< "$x" | wc -l`
+echo "Muestra el número de caracteres igual a -$2- que contenga la palabra dada"
 for ((numero=1; numero<11; numero ++)); do
-        echo "La palabra $x contiene $veces caracteres igual a -a-"
+        echo "La palabra $x contiene $veces caracteres igual a -$2-"
         break 1
 done
 
