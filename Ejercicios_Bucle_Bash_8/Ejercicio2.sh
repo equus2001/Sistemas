000e475fe106a1f66b4125cdaea6cdeff5b85c79
equus2001 <62957466+equus2001@users.noreply.github.com>
2020-05-11 20:02:02 +0200

    Update Ejercicio2.sh

--- Ejercicios_Bucle_Bash_8/Ejercicio2.sh
+++ Ejercicios_Bucle_Bash_8/Ejercicio2.sh
@@ -6,6 +6,8 @@ echo "Muestra el número de caracteres igual a -$2- que contenga la palabra dada
 for ((numero=10; numero<11; numero ++)); do
 if [ $longitud -le $numero ];then
         echo "La palabra $x contiene $veces caracteres igual a -$2-"
+else
+        echo "Supera los 10 carácteres"
 fi
         break 1
 done
