354f7daaebd453384e19a98ff2dc66f5ae5ff48b
equus2001 <62957466+equus2001@users.noreply.github.com>
2020-05-11 19:50:46 +0200

    Update Ejercicio2.sh

--- Ejercicios_Bucle_Bash_8/Ejercicio2.sh
+++ Ejercicios_Bucle_Bash_8/Ejercicio2.sh
@@ -4,7 +4,9 @@ longitud=${#x}
 veces=`grep -o "$2" <<< "$x" | wc -l`
 echo "Muestra el número de caracteres igual a -$2- que contenga la palabra dada"
 for ((numero=1; numero<11; numero ++)); do
+if [ $longitud -le $numero ];then
         echo "La palabra $x contiene $veces caracteres igual a -$2-"
+fi
         break 1
 done
 
